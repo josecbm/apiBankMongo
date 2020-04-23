641536a25003fe3b89a60542f1fce03f646b1058
josecbm <jcbautista95@gmail.com>
2020-04-22 22:08:33 -0600

    prueba heroku 2

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ connectDB()
 app.use(express.json({extended:false}));
 app.use('/api/userModel',require('./Api/User'));
 app.use('/api/transModel',require('./Api/Transferencia'));
+
+app.get('/',function (req , res){
+    res.send('Bienvenido a la consola de heroku'); 
+});
 const port = process.env.PORT || 3000;
 
 app.listen(port ,()=>console.log("Servidor levantado"));
